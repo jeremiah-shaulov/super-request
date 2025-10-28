f08e9118df822a9a66419d27c3124bc116e2cde5
jeremiah-shaulov <jeremiah.shaulov@gmail.com>
2025-10-28 23:35:05 +0300

    To water@1.1.1

--- private/deps.ts
+++ private/deps.ts
@@ -1,2 +1,2 @@
 export {default as Iconv} from 'npm:iconv-lite@0.7.0';
-export {RdStream, CancelError} from 'jsr:@shaulov/water@1.1.0';
+export {RdStream, CancelError} from 'jsr:@shaulov/water@1.1.1';
